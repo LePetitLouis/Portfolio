9e8f5e4c54b20b5c544da85665b867c5e8b6f22b
LePetitLouis <44061639+LePetitLouis@users.noreply.github.com>
2020-10-13 10:31:12 +0200

    All

---
+++ script/app.js
@@ -0,0 +1,51 @@
+// Menu NavBar
+var marker = document.querySelector('#marker');
+var item = document.querySelectorAll('ul li a');
+
+function indicator(e){
+    marker.style.left = e.offsetLeft+"px";
+    marker.style.width = e.offsetWidth+"px";
+}
+
+item.forEach(element => {
+    element.addEventListener('mouseover', (e)=>{
+        indicator(e.target);    
+    })
+});
+
+// Scroll lien Nav
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
